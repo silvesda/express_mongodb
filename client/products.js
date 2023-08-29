6afb1743ac05bd438d8fa65b14d3def9c55f45c3
silvesda <133907138+silvesda@users.noreply.github.com>
2023-08-30 02:14:37 +0500

    product.js to generate products for products page
    added product list and using it to create products using createProductItem function

---
+++ client/products.js
@@ -0,0 +1,45 @@
+const products = [
+    {
+      "title": "Fishing Rod",
+      "image": "rod.jpg",
+      "description": "A high-quality fishing rod for all your angling adventures.",
+      "price": "$99.99"
+    },
+    {
+      "title": "Tackle Box",
+      "image": "tackle-box.jpg",
+      "description": "A spacious tackle box to keep your fishing gear organized.",
+      "price": "$39.99"
+    },
+
+  ]
+  
+
+  function createProductItem(product) {
+    const productItem = document.createElement("div");
+    productItem.className = "product-item";
+  
+    const image = document.createElement("img");
+    image.src = product.image;
+    image.alt = product.title;
+  
+    const title = document.createElement("div");
+    title.className = "product-title";
+    title.textContent = product.title;
+  
+    const description = document.createElement("div");
+    description.className = "product-description";
+    description.textContent = product.description;
+  
+    const price = document.createElement("div");
+    price.className = "product-price";
+    price.textContent = product.price;
+  
+    productItem.appendChild(image);
+    productItem.appendChild(title);
+    productItem.appendChild(description);
+    productItem.appendChild(price);
+  
+    return productItem;
+  }
+  
