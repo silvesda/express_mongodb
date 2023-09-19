11d141c6a929fa6353037f1eeb277cbf8f2e9db6
silvesda <133907138+silvesda@users.noreply.github.com>
2023-09-19 13:37:28 +0500

    data validation before sending data

--- client/contact.js
+++ client/contact.js
@@ -4,6 +4,23 @@ const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 const submitButton = document.getElementById('submit-button');
 
+const validationErrors = document.getElementById('validation-errors');
+const successMessage = document.getElementById('success-message');
+
+
+function displayValidationErrors(errors) {
+  validationErrors.innerHTML = '<ul><li>' + errors.join('</li><li>') + '</li></ul>';
+  validationErrors.style.display = 'block';
+}
+
+function hideValidationErrors() {
+  validationErrors.style.display = 'none';
+}
+
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
 
 function displaySuccessMessage() {
   const successMessage = document.getElementById('success-message');
@@ -14,19 +31,37 @@ function displaySuccessMessage() {
 async function handleSubmit(event) {
   event.preventDefault();
 
+  hideValidationErrors();
+
   const name = nameInput.value;
   const email = emailInput.value;
   const message = messageInput.value;
 
-  try {
+  const errors = [];
 
-    await submitContactForm(name, email, message);
+  if (name.trim() === '') {
+    errors.push('Please enter your name.');
+  }
 
+  if (email.trim() === '') {
+    errors.push('Please enter your email address.');
+  } else if (!isValidEmail(email)) {
+    errors.push('Please enter a valid email address.');
+  }
 
-    displaySuccessMessage();
-  } catch (error) {
-    console.error('Error:', error.message);
+  if (message.trim() === '') {
+    errors.push('Please enter your message.');
+  }
 
+  if (errors.length > 0) {
+    displayValidationErrors(errors);
+  } else {
+    try {
+      await submitContactForm(name, email, message);
+      displaySuccessMessage();
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
   }
 }
 
